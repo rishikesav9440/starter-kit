157f39eda5cac73cad07af930246cb650f6725bc
Rishi Kesav <121454665+rishikesav9440@users.noreply.github.com>
2024-10-07 18:00:03 +0530

    Update post-page-navbar.tsx

--- packages/blog-starter-kit/themes/hashnode/components/post-page-navbar.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/post-page-navbar.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import Link from 'next/link';
+import { forwardRef } from 'react';
+import { twJoin } from 'tailwind-merge';
 
-const PostPageNavbar = ({ publication }) => {
-  return (
-    <nav>
-      {/* Navigation content goes here */}
-    </nav>
-  );
+/* eslint-disable no-nested-ternary */
+import { getCommonBtnStyles } from './common-header-icon-btn';
+import HeaderBlogSearch from './header-blog-search';
+import HeaderLeftSidebar from './header-left-sidebar';
+import HeaderTooltip from './header-tooltip';
+import { ChevronLeftSVG } from './icons/svgs/';
+import PublicationSocialLinks from './publication-social-links';
+import useStickyNavScroll from './use-sticky-nav-scroll';
+
+import { PublicationFragment } from '../generated/graphql';
+import { Button } from './custom-button';
+import PublicationLogo from './publication-logo';
+
+type Props = {
+	publication: Pick<PublicationFragment, 'id' | 'title' | 'links' | 'url' | 'features' | 'isTeam' | 'author' | 'preferences'>;
 };
+
+const PostPageNavbar = forwardRef<HTMLElement, Props>((props, ref) => {
+	const { publication } = props;
+
+	useStickyNavScroll({ elRef: ref });
+
+	const commonIconBtnStyles = getCommonBtnStyles();
+
+	return (
+		<div className="container mx-auto px-2 md:px-4 md:py-1 2xl:px-10">
+		
+
+			{/* Logo for mobile view */}
+			
+
+		
+		</div>
+	);
+});
+
+PostPageNavbar.displayName = 'PostPageNavbar';
 
 export default PostPageNavbar;
